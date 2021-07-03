f42b83c33ebe3da5e7a7dbe6d7c2623567542c77
Alexey Ya <you@example.com>
2021-07-03 13:04:39 +0300

    slider из секции ворк исправлен

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -6,19 +6,26 @@ const thumbs = {
 };
 
 const btns = {
-  template: "#slider-btns"
+  template: "#slider-btns",
 };
 
 const display = {
-  props: ["currentWork", "works"],
+  props: ["currentWork", "works", "currentIndex"],
   template: "#slider-display",
   components: {
     thumbs,
     btns
+  },
+  computed: {
+    reversedWorks() {
+      const works = [...this.works];
+      return works.slice(0, 3).reverse();
+    }
   }
 };
 
 const tags = {
+  props: ["tags"],
   template: "#slider-tags"
 };
 
@@ -27,6 +34,11 @@ const info = {
   template: "#slider-info",
   components: {
     tags
+  },
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(",");
+    }
   }
 };
 
@@ -40,21 +52,50 @@ new Vue({
   data() { // записываем данные в объект data, чтобы обращаться к ним внутри компонента
     return {
       works: [],
-      currentWork: {}
+      currentIndex: 0
     };
   },
+  computed: {
+    currentWork() {
+      return this.works[0];
+    }
+  },
+  watch: {
+    currentIndex(value) {
+      this.makeInfiniteLoopForNdx(value);
+    }
+  },
   methods: {
+    makeInfiniteLoopForNdx(index) {
+      const workNumber = this.works.length - 1
+      if (index < 0) this.currentIndex = this.works.length - 1;
+      if (index > workNumber) this.currentIndex = 0;
+    },
     requireImagesToArray(data) {
       return data.map(item => {
         const requireImages = require(`../images/content/${item.photo}`).default;
         item.photo = requireImages;
         return item;
       });
+    },
+    slide(direction) {
+      const lastItem = this.works[this.works.length - 1]; // последний элемент в слайдере
+      switch(direction) {
+        case "next":
+          this.works.push(this.works[0]);
+          this.works.shift();
+          this.currentIndex++;
+          break;
+        case "prev":
+          this.works.unshift(lastItem);
+          this.works.pop();
+          this.currentIndex--;
+          break;
+      }
     }
   },
   created() { // запросить данные, не обращаясь к реальным дом-узлам
     const data = require("../data/works.json");
     this.works = this.requireImagesToArray(data);
-    this.currentWork = this.works[0];
   }
 });

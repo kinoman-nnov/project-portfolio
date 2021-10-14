5ae1f60387b3276001f4f1cc9e955469c2fdbe7e
Алексей Витальевич Якимов <yakimovav@salut27.ru>
2021-10-14 16:15:35 +0300

    добавил роутер и страницы

---
+++ src/admin/pages/about/about.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="about-page-components">  
+    <div class="page-content">
+      <div class="container">
+        <div class="header">
+          <div class="title">Блок "Обо мне"</div>
+          <iconed-button 
+            type="iconed" 
+            v-if="emptyCatIsShown === false"
+            @click="emptyCatIsShown = true" 
+            title="Добавить группу"
+          />
+        </div>
+        <ul class="skills">
+          <li class="item" v-if="emptyCatIsShown">
+            <category 
+              @remove="emptyCatIsShown = false"
+              empty 
+            />
+          </li>
+          <li 
+            class="item" 
+            v-for="category in categories"
+            :key="category.id"
+          >
+            <category 
+              :title="category.category"
+              :skills="category.skills"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+import headline from "../../components/headline";
+import user from "../../components/user";
+import navigation from "../../components/navigation";
+import button from "../../components/button";
+import category from "../../components/category";
+
+export default {
+  components: {
+    headline,
+    user,
+    navigation,
+    iconedButton: button,
+    category
+  },
+  data() {
+    return {
+      categories: [],
+      emptyCatIsShown: false
+    }
+  },
+  created() {
+    this.categories = require("../../data/categories.json");
+  }
+};
+</script>
+
+<style lang="postcss" scoped src="./about.pcss"></style>
---
+++ src/admin/pages/login/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-page-component">
+    <div class="content">
+      <form class="form" @submit.prevent="handleSubmit">
+        <div class="form-title">Авторизация</div>
+        <div class="row">
+          <app-input
+            title="Логин"
+            icon="user"
+          />
+        </div>
+        <div class="row">
+          <app-input
+            title="Пароль"
+            icon="key"
+            type="password"
+          />
+        </div>
+        <div class="btn">
+          <app-button typeAttr="submit" title="Отправить" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import appInput from "../../components/input";
+import appButton from "../../components/button";
+
+export default {
+  data: () => ({}),
+  components: { appInput, appButton },
+}
+</script>
+
+<style lang="postcss" scoped src="./login.pcss"></style>

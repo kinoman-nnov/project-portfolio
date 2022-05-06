e785323fdbe10fe317b348ebda694c203194fc09
Алексей Витальевич Якимов <yakimovav@salut27.ru>
2022-05-06 15:00:31 +0300

    работаю над проверкой пользователя

--- src/admin/router.js
+++ src/admin/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from './store';
+import axios from "axios";
 
 Vue.use(VueRouter);
 
@@ -34,7 +36,40 @@ const routes = [
   {
     path: '/login',
     component: login,
+    meta: {
+      public: true
+    }
   }
 ];
 
-export default new VueRouter({ routes });
+const router = new VueRouter({ routes });
+
+const guard = axios.create({
+  baseURL: "https://webdev-api.loftschool.com"
+});
+
+router.beforeEach(async(to, from, next) => {
+  const isPublicRoute = to.matched.some(route => route.meta.public);
+  console.log(isPublicRoute);
+  const isUserLoggedIn = store.getters["user/userIsLoggedIn"];
+  
+  if (isPublicRoute === false && isUserLoggedIn === false) {
+    const token = localStorage.getItem("token");
+    
+    guard.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+    try {
+      const response = await guard.get("/user");
+      store.dispatch("user/login", await response.data.user);
+
+      next();
+    } catch (error) {
+      router.replace("/login");
+      localStorage.removeItem("token");
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
